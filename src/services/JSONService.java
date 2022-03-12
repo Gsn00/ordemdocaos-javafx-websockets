fce0d64c816f0031d9510d89e0f4834c91ceca73
Gabriel Novais <silvanovaesgabriel@gmail.com>
2022-03-12 17:40:55 -0300

    Removed useless things from JSONService

--- src/services/JSONService.java
+++ src/services/JSONService.java
@@ -10,8 +10,6 @@ import java.util.List;
 import com.github.cliftonlabs.json_simple.JsonObject;
 import com.github.cliftonlabs.json_simple.Jsoner;
 
-import javafx.application.Platform;
-
 public class JSONService
 {
 
@@ -84,8 +82,6 @@ public class JSONService
 			} catch (Exception e)
 			{
 				e.printStackTrace();
-				System.exit(0);
-				Platform.exit();
 			}
 		}
 		return null;
